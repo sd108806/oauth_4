b06906e9a1f1ff007e9215981d29aaecd349c13a
sd108806 <sd108806@ohio.edu>
2014-10-20 09:06:04 -0400

    change style

---
+++ test_oauth/main.js
@@ -0,0 +1,19 @@
+
+
+function main()
+
+{
+
+get_oauth();
+
+
+
+var obj_event;
+get_event( obj_event );
+
+document.write('obj_event.events[0].node  ' + obj_event.events[0].node+ '<br/>');
+
+document.write('obj_event.events[0].description.commits[0].description ' + obj_event.events[0].description.commits[0].description+ '<br/>');
+
+
+}
---
+++ test_oauth/test_jsonform.js
@@ -0,0 +1,32 @@
+
+
+
+function test_jsonform()
+{
+
+var newobj= {
+'count':10,
+
+'events':[
+{'node':1,
+'color':'blue'},
+
+
+
+
+{'node':3,
+'color':'green'}
+
+]
+
+}
+
+document.write('newobj.count' + newobj.count +'<br/>'+'<br/>');
+
+document.write('newobj.events[0].node  ' + newobj.events[0].node +'<br/>'+'<br/>');
+
+document.write('newobj.events[1].node  ' + newobj.events[1].node +'<br/>'+'<br/>');
+
+
+
+}
